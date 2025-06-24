33c7e8a352544ad4476f35e72aeac5f2eb04bd27
sidsurakanti <sidsurakanti1@gmail.com>
2025-06-23 21:01:04 -0700

    feat: can do a lab around track (added collision stuff)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
+
 import * as THREE from "three";
 import { Suspense, useEffect, useRef } from "react";
-import { useCarStore, type CarState } from "@/lib/store";
+import { useCarStore } from "@/lib/store";
 import { Canvas, useFrame } from "@react-three/fiber";
 import {
   Environment,
@@ -9,7 +10,12 @@ import {
   useGLTF,
   useHelper,
 } from "@react-three/drei";
-import { Physics, RigidBody } from "@react-three/rapier";
+import {
+  Physics,
+  RapierRigidBody,
+  RigidBody,
+  CuboidCollider,
+} from "@react-three/rapier";
 
 export default function Home() {
   return (
@@ -18,56 +24,110 @@ export default function Home() {
         <Canvas
           camera={{ fov: 80, near: 0.1, far: 1000, position: [0, 20, 15] }}
         >
+          <fog attach="fog" args={[0xcfecf7, 45, 200]} />
+          <Environment preset="sunset" />
+          <ambientLight intensity={3.5} />
+          {/* <gridHelper args={[500, 30, 30]} /> */}
+          <axesHelper args={[50]} />
           <Suspense fallback={<></>}>
             <Physics debug>
-              <Environment preset="sunset" />
-              <ambientLight intensity={3.5} />
-              <gridHelper args={[500, 30, 30]} />
-              <axesHelper args={[50]} />
               <RigidBody type="fixed" colliders="cuboid">
-                <mesh
-                  rotation={[-Math.PI / 2, 0, 0]}
-                  position={[0, -1, 0]}
-                  // receiveShadow
-                >
+                <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
                   <planeGeometry args={[500, 500]} />
-                  <meshStandardMaterial color={"gray"} />
+                  <meshStandardMaterial color={"#88e788"} />
                 </mesh>
               </RigidBody>
               <Light position={[0, 5, 0]} intensity={0.5} />
-              <Car position={[0, 0, 0]} />
+              <Car />
               <Track />
-              <Cube />
-              <OrbitControls />
+              {/* <Cube /> */}
             </Physics>
           </Suspense>
+          <OrbitControls />
         </Canvas>
       </main>
     </>
   );
 }
-function Light(props: React.JSX.IntrinsicElements["directionalLight"]) {
-  const lightRef = useRef<THREE.DirectionalLight | null>(null);
-  // useHelper(
-  //   lightRef as RefObject<THREE.DirectionalLight>,
-  //   THREE.DirectionalLightHelper,
-  //   1,
-  // );
 
-  return <directionalLight ref={lightRef} {...props} />;
-}
+function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
+  const { scene } = useGLTF("/models/car.glb");
+  const bodyRef = useRef<RapierRigidBody>(null);
+  const turnStep = Math.PI / 18;
+  const { direction, velocity, setVelocity, setDirection } = useCarStore(
+    (state) => state,
+  );
 
-function Cube() {
-  const meshRef = useRef<THREE.Mesh>(null);
-  useFrame((state, delta) => {
-    if (meshRef.current) meshRef.current.rotation.y += delta;
+  useEffect(() => {
+    window.onkeydown = (e: KeyboardEvent) => {
+      switch (e.key) {
+        case "w":
+          setVelocity(0.015);
+          break;
+        case "s":
+          setVelocity(-0.015);
+          break;
+        case "d":
+          setDirection(-turnStep);
+          break;
+        case "a":
+          setDirection(+turnStep);
+          break;
+        case "r":
+          if (!bodyRef.current) break;
+          bodyRef.current.setTranslation({ x: 5, y: 0, z: 0 }, true);
+          bodyRef.current.setLinvel({ x: 0, y: 0, z: 0 }, true);
+          break;
+      }
+    };
+  }, []);
+
+  useFrame(({ camera }, _) => {
+    if (!bodyRef.current) return;
+    const body = bodyRef.current;
+
+    const quat = new THREE.Quaternion();
+    quat.setFromEuler(new THREE.Euler(0, direction, 0));
+    body.setRotation(quat, true);
+
+    const curvedVelocity = Math.tanh(velocity * 2.5) * 7.5;
+    body.setLinvel(
+      new THREE.Vector3(
+        -Math.sin(direction) * curvedVelocity,
+        0,
+        -Math.cos(direction) * curvedVelocity,
+      ),
+      true,
+    );
+    setVelocity(-0.001);
+
+    const camOffset = new THREE.Vector3(0, 2.5, 6);
+    camOffset.applyQuaternion(quat);
+
+    const pos = body.translation();
+    camera.lookAt(pos.x, pos.y, pos.z);
+    camera.position.set(
+      pos.x + camOffset.x,
+      pos.y + camOffset.y,
+      pos.z + camOffset.z,
+    );
   });
 
   return (
-    <mesh ref={meshRef} position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-      <boxGeometry />
-      <meshStandardMaterial color={"#ffffff"} />
-    </mesh>
+    <RigidBody
+      enabledRotations={[false, true, false]}
+      position={[5, 0, 0]}
+      restitution={0}
+      linearDamping={0.25}
+      angularDamping={0.25}
+      friction={0}
+      type="dynamic"
+      colliders={false}
+      ref={bodyRef}
+    >
+      <primitive {...props} object={scene} scale={1} rotation={[0, 0, 0]} />
+      <CuboidCollider args={[1.0, 0.5, 1.5]} position={[0, 0.5, 0]} />
+    </RigidBody>
   );
 }
 
@@ -81,72 +141,34 @@ function Track(
       <primitive
         {...props}
         object={scene}
-        scale={0.005}
-        position={[50, 0.1, 0]}
+        scale={0.0065}
+        position={[65, 0.1, 0]}
       />
     </RigidBody>
   );
 }
 
-function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
-  const { scene } = useGLTF("/models/car.glb");
+function Light(props: React.JSX.IntrinsicElements["directionalLight"]) {
+  const lightRef = useRef<THREE.DirectionalLight | null>(null);
+  // useHelper(
+  //   lightRef as RefObject<THREE.DirectionalLight>,
+  //   THREE.DirectionalLightHelper,
+  //   1,
+  // );
 
-  const carRef = useRef<THREE.Mesh | null>(null);
-  const {
-    position,
-    direction,
-    velocity,
-    update,
-    setPosition,
-    setVelocity,
-    setDirection,
-  } = useCarStore((state) => state);
-  const turnStep = Math.PI / 20;
+  return <directionalLight ref={lightRef} {...props} />;
+}
 
-  useEffect(() => {
-    window.onkeydown = (e: KeyboardEvent) => {
-      switch (e.key) {
-        case "w":
-          setVelocity(0.1);
-          break;
-        case "s":
-          setVelocity(0);
-          break;
-        case "d":
-          setDirection(-turnStep);
-          break;
-        case "a":
-          setDirection(+turnStep);
-          break;
-        case "r":
-          setPosition({ x: 0, y: 0, z: 0 });
-          break;
-      }
-    };
-  }, []);
-
-  useFrame(({ camera }, delta) => {
-    if (carRef.current) {
-      const carPos = carRef.current.position;
-      update(delta);
-      carPos.x = position.x;
-      carPos.z = position.z;
-      // console.log(carRef.current.position);
-      // console.log(camera.position);
-      camera.lookAt(carPos);
-      camera.position.set(carPos.x, carPos.y + 3, carPos.z + 5);
-      carRef.current.rotation.y = direction;
-    }
+function Cube() {
+  const meshRef = useRef<THREE.Mesh>(null);
+  useFrame((_, delta) => {
+    if (meshRef.current) meshRef.current.rotation.y += delta;
   });
 
   return (
-    <primitive
-      {...props}
-      ref={carRef}
-      object={scene}
-      scale={1}
-      position={[10, 0, 0]}
-      rotation={[0, 0, 0]}
-    />
+    <mesh ref={meshRef} position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+      <boxGeometry />
+      <meshStandardMaterial color={"#ffffff"} />
+    </mesh>
   );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -3,38 +3,22 @@ import { create } from "zustand";
 export type CarState = {
   velocity: number;
   direction: number;
-  position: { x: number; y: number; z: number };
   setVelocity: (n: number) => void;
   setDirection: (n: number) => void;
-  setPosition: ({ x, y, z }: { x: number; y: number; z: number }) => void;
-  update: (delta: number) => void;
 };
 
 export const useCarStore = create<CarState>()((set) => ({
   velocity: 0,
   direction: 0,
-  position: {
-    x: 0,
-    y: 0,
-    z: 0,
-  },
   setVelocity: (n) =>
     set((state) => ({
-      velocity: n > 0 ? Math.tanh((state.velocity + n) * 0.1) * 10 : 0,
+      velocity:
+        n >= 0
+          ? Math.min(state.velocity + n, 1)
+          : Math.max(state.velocity + n, 0),
     })),
   setDirection: (n) =>
     set((state) => ({
       direction: state.direction + n,
-    })),
-  setPosition: ({ x, y, z }) => set({ position: { x: x, y: y, z: z } }),
-  update: (delta) =>
-    set((state) => ({
-      position: {
-        x:
-          state.position.x - Math.sin(state.direction) * delta * state.velocity,
-        y: state.position.y,
-        z:
-          state.position.z - Math.cos(state.direction) * delta * state.velocity,
-      },
     })),
 }));
