6b175bac6260dde8c70ae19125608d6205a4d1a8
sidsurakanti <sidsurakanti1@gmail.com>
2025-06-26 02:07:38 -0700

    feat: no more monaco but we move on

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,7 @@ import { Suspense, useEffect, useRef } from "react";
 import { cn } from "@/lib/utils";
 import { useCarStore } from "@/lib/store";
 import { Canvas, useFrame } from "@react-three/fiber";
-import {
-  Environment,
-  OrbitControls,
-  useGLTF,
-  useHelper,
-} from "@react-three/drei";
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
 import {
   Physics,
   RapierRigidBody,
@@ -23,60 +18,23 @@ export default function Home() {
   return (
     <>
       <main className="relative w-full h-screen">
-        <section className="flex flex-col space-y-1 p-2 m-2 z-1 absolute bottom-0 right-0 bg-white/60 backdrop-blur">
-          <span className="flex items-center gap-1.5">
-            <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
-              w
-            </kbd>{" "}
-            forward
-          </span>
-          <span className="flex items-center gap-1.5">
-            <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
-              s
-            </kbd>{" "}
-            brake
-          </span>
-          <span className="flex items-center gap-1.5">
-            <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
-              d
-            </kbd>{" "}
-            right
-          </span>
-          <span className="flex items-center gap-1.5">
-            <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
-              a
-            </kbd>{" "}
-            left
-          </span>
-          <span className="flex items-center gap-1.5">
-            <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
-              ⎵
-            </kbd>{" "}
-            DRS
-          </span>
-          <span className="flex items-center gap-1.5">
-            <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
-              r
-            </kbd>{" "}
-            reset
-          </span>
-        </section>
         <Hud />
+        <Help />
 
         <Canvas
-          camera={{ fov: 80, near: 0.1, far: 1000, position: [0, 20, 15] }}
+          camera={{ fov: 85, near: 0.1, far: 1000, position: [10, 20, 15] }}
         >
-          <fog attach="fog" args={[0xcfecf7, 50, 200]} />
+          <fog attach="fog" args={[0xcfecf7, 100, 400]} />
           <Environment preset="sunset" />
-          <ambientLight intensity={2.5} />
+          <ambientLight intensity={0.8} />
           {/* <gridHelper args={[500, 30, 30]} /> */}
           {/* <axesHelper args={[50]} /> */}
-          <Suspense fallback={<></>}>
-            <Physics gravity={[0, -30, 0]}>
+          <Physics>
+            <Suspense fallback={<></>}>
               <Car />
               <Track />
-            </Physics>
-          </Suspense>
+            </Suspense>
+          </Physics>
           <OrbitControls />
         </Canvas>
       </main>
@@ -86,45 +44,51 @@ export default function Home() {
 
 function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
   const { scene } = useGLTF("/models/f1.glb");
-  const bodyRef = useRef<RapierRigidBody>(null);
-  const isMoving = useRef<boolean>(false);
-  const isBoosting = useRef<number>(0);
-  const turnStep = Math.PI / 18;
-  const lookTarget = useRef(new THREE.Vector3());
   const {
     direction,
     velocity,
+    boost,
+    setBoost,
     resetDirection,
     resetVelocity,
     setVelocity,
     setDirection,
   } = useCarStore((state) => state);
-  const INITIAL_DIRECTION = -6 * turnStep;
-  const INITIAL_POSITION: [x: number, y: number, z: number] = [15, 0, 0];
+
+  const bodyRef = useRef<RapierRigidBody>(null);
+  const isMoving = useRef<boolean>(false);
+  const isBoosting = useRef<number>(0);
   const initialLoad = useRef<boolean>(false);
+  const lookTarget = useRef(new THREE.Vector3());
+
+  const TURN_STEP = Math.PI / 16;
+  const INITIAL_DIRECTION = -0 * TURN_STEP;
+  const INITIAL_POSITION: [x: number, y: number, z: number] = [15, 0, 0];
 
   useEffect(() => {
     if (bodyRef.current && !initialLoad.current) {
-      const quat = new THREE.Quaternion();
+      initialLoad.current = true;
       setDirection(INITIAL_DIRECTION);
+
+      const quat = new THREE.Quaternion();
       quat.setFromEuler(new THREE.Euler(0, direction, 0));
       bodyRef.current.setRotation(quat, true);
-      initialLoad.current = true;
     }
+
     const onKeyDown = (e: KeyboardEvent) => {
       switch (e.key) {
         case "w":
-          setVelocity(0.1);
+          setVelocity(0.065);
           isMoving.current = true;
           break;
         case "s":
-          setVelocity(-0.1);
+          isMoving.current = false;
           break;
         case "d":
-          setDirection(-turnStep);
+          setDirection(-TURN_STEP);
           break;
         case "a":
-          setDirection(+turnStep);
+          setDirection(+TURN_STEP);
           break;
         case "r":
           if (!bodyRef.current) break;
@@ -149,14 +113,15 @@ function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
 
           break;
         case " ":
-          isBoosting.current = 20;
+          if (isMoving.current) setVelocity(0.065);
+          setBoost(true);
           break;
       }
     };
 
     const onKeyUp = (e: KeyboardEvent) => {
       if (e.key === "w") isMoving.current = false;
-      if (e.key === " ") isBoosting.current = 0;
+      if (e.key === " ") setBoost(false);
     };
 
     window.addEventListener("keydown", onKeyDown);
@@ -171,13 +136,39 @@ function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
     if (!bodyRef.current) return;
     const body = bodyRef.current;
 
-    const quat = new THREE.Quaternion();
-    quat.setFromEuler(new THREE.Euler(0, direction, 0));
-    body.setRotation(quat, true);
+    const currRot = body.rotation(); // returns {x, y, z, w}
+    const currQuat = new THREE.Quaternion(
+      currRot.x,
+      currRot.y,
+      currRot.z,
+      currRot.w,
+    );
 
-    // const curvedVelocity =
-    //   Math.tanh(velocity * 2.5) * 16.5 + isBoosting.current;
-    const curvedVelocity = Math.pow(velocity, 3) * 50 + isBoosting.current;
+    const targetQuat = new THREE.Quaternion();
+    targetQuat.setFromEuler(new THREE.Euler(0, direction, 0));
+
+    const smoothedQuat = currQuat.clone(); // clone so we don’t mutate the original
+    smoothedQuat.slerp(targetQuat, 0.4);
+
+    body.setRotation(
+      {
+        x: smoothedQuat.x,
+        y: smoothedQuat.y,
+        z: smoothedQuat.z,
+        w: smoothedQuat.w,
+      },
+      true,
+    );
+
+    const TOP_SPEED = 80;
+    const MID_POINT_T = 1;
+    const ACCEL_SPEED = 1.5;
+    const curvedVelocity =
+      (TOP_SPEED * Math.pow(velocity, ACCEL_SPEED)) /
+        (Math.pow(velocity, ACCEL_SPEED) + Math.pow(MID_POINT_T, ACCEL_SPEED)) +
+      boost;
+    // const curvedVelocity = Math.tanh(velocity * 2.5) * 70 + isBoosting.current;
+    // const curvedVelocity = Math.pow(velocity, 3) * 50 + isBoosting.current;
     body.setLinvel(
       new THREE.Vector3(
         -Math.sin(direction) * curvedVelocity,
@@ -186,14 +177,14 @@ function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
       ),
       true,
     );
-    // if (isMoving.current) setVelocity(-0.1);
 
     // add downforce lmfao
+    // this was for my other track with different elevations but keeping it here for trolls
     const downforce = -Math.pow(curvedVelocity, 1.2) * 0.5;
     body.applyImpulse({ x: 0, y: downforce, z: 0 }, true);
 
-    const camOffset = new THREE.Vector3(0, 10, 20);
-    camOffset.applyQuaternion(quat);
+    const camOffset = new THREE.Vector3(0, 10, 17);
+    camOffset.applyQuaternion(targetQuat);
 
     const pos = body.translation();
     const targetPos = new THREE.Vector3(
@@ -201,12 +192,12 @@ function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
       pos.y + camOffset.y,
       pos.z + camOffset.z,
     );
-    camera.position.lerp(targetPos, 0.05);
+    camera.position.lerp(targetPos, 0.2);
 
     const target = lookTarget.current;
-    target.x = MathUtils.lerp(target.x, pos.x, 0.05);
-    target.y = MathUtils.lerp(target.y, pos.y, 0.05);
-    target.z = MathUtils.lerp(target.z, pos.z, 0.05);
+    target.x = MathUtils.lerp(target.x, pos.x, 0.2);
+    target.y = MathUtils.lerp(target.y, pos.y, 0.2);
+    target.z = MathUtils.lerp(target.z, pos.z, 0.2);
     camera.lookAt(target.x, target.y, target.z);
   });
 
@@ -215,8 +206,6 @@ function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
       ccd={true}
       position={INITIAL_POSITION}
       restitution={0}
-      linearDamping={0.9}
-      angularDamping={0.5}
       friction={0}
       type="dynamic"
       colliders={false}
@@ -235,12 +224,22 @@ function Car(props: Omit<React.JSX.IntrinsicElements["primitive"], "object">) {
 }
 
 function Hud() {
-  const { velocity } = useCarStore((state) => state);
+  const { velocity, boost } = useCarStore((state) => state);
   const bars = 10;
-  const active = Math.round(velocity * bars);
+  const TOP_SPEED = 200;
+  const MID_POINT_T = 1;
+  const ACCEL_SPEED = 1.5;
+  const curvedVelocity =
+    (TOP_SPEED * Math.pow(velocity, ACCEL_SPEED)) /
+      (Math.pow(velocity, ACCEL_SPEED) + Math.pow(MID_POINT_T, ACCEL_SPEED)) +
+    boost;
+  const active = Math.round(velocity * 0.5 * bars);
   return (
-    <div className="absolute top-0 w-full flex justify-center auto z-1">
-      <span className="gap-1 p-2 m-2 flex bg-black/20 text-white text-2xl ">
+    <div className="absolute top-0 w-full flex justify-center items-center gap-5 z-1">
+      <p className="tracking-tighter text-2xl font-bold">
+        {Math.floor(curvedVelocity)} m/ph{" "}
+      </p>
+      <span className="gap-1 p-2 m-2 flex bg-black/20 text-white text-2xl shadow">
         {Array.from({ length: bars }).map((_, i) => (
           <div
             key={i}
@@ -252,6 +251,11 @@ function Hud() {
           />
         ))}
       </span>
+      <p
+        className={cn(boost > 0 ? "text-emerald-700 font-bold" : "", "text-xl")}
+      >
+        DRS{" "}
+      </p>
     </div>
   );
 }
@@ -259,45 +263,55 @@ function Hud() {
 function Track(
   props: Omit<React.JSX.IntrinsicElements["primitive"], "object">,
 ) {
-  const { scene } = useGLTF("/models/monaco.glb");
-  // scale={6}
-  // position={[-360, -10, 0]}
-  // rotation={[Math.PI, Math.PI, 0]}
-  // const { scene } = useGLTF("/models/track2.glb");
+  const { scene } = useGLTF("/models/track.glb");
 
   return (
-    <RigidBody friction={0} restitution={0} type="fixed" colliders="trimesh">
-      <primitive
-        {...props}
-        object={scene}
-        scale={0.025}
-        position={[250, 0.1, 0]}
-      />
+    <RigidBody friction={0} restitution={0} type="fixed" colliders={false}>
+      <primitive {...props} object={scene} scale={5.75} />
+      <CuboidCollider args={[800, 0.1, 800]} position={[0, -0.075, 0]} />
     </RigidBody>
   );
 }
 
-function Light(props: React.JSX.IntrinsicElements["directionalLight"]) {
-  const lightRef = useRef<THREE.DirectionalLight | null>(null);
-  // useHelper(
-  //   lightRef as RefObject<THREE.DirectionalLight>,
-  //   THREE.DirectionalLightHelper,
-  //   1,
-  // );
-
-  return <directionalLight ref={lightRef} {...props} />;
-}
-
-function Cube() {
-  const meshRef = useRef<THREE.Mesh>(null);
-  useFrame((_, delta) => {
-    if (meshRef.current) meshRef.current.rotation.y += delta;
-  });
-
+function Help() {
   return (
-    <mesh ref={meshRef} position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
-      <boxGeometry />
-      <meshStandardMaterial color={"#ffffff"} />
-    </mesh>
+    <section className="flex flex-col space-y-1 p-2 m-2 z-1 absolute bottom-0 right-0 bg-white/60 backdrop-blur">
+      <span className="flex items-center gap-1.5">
+        <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
+          w
+        </kbd>{" "}
+        forward
+      </span>
+      <span className="flex items-center gap-1.5">
+        <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
+          s
+        </kbd>{" "}
+        brake
+      </span>
+      <span className="flex items-center gap-1.5">
+        <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
+          d
+        </kbd>{" "}
+        right
+      </span>
+      <span className="flex items-center gap-1.5">
+        <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
+          a
+        </kbd>{" "}
+        left
+      </span>
+      <span className="flex items-center gap-1.5">
+        <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
+          ⎵
+        </kbd>{" "}
+        DRS
+      </span>
+      <span className="flex items-center gap-1.5">
+        <kbd className="py-0.25 px-2 shadow-md border border-stone-400 rounded-lg bg-stone-200">
+          r
+        </kbd>{" "}
+        reset
+      </span>
+    </section>
   );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 export type CarState = {
   velocity: number;
   direction: number;
+  boost: number;
+  setBoost: (n: boolean) => void;
   resetVelocity: () => void;
   resetDirection: () => void;
   setVelocity: (n: number) => void;
@@ -12,13 +14,15 @@ export type CarState = {
 export const useCarStore = create<CarState>()((set) => ({
   velocity: 0,
   direction: 0,
+  boost: 0,
+  setBoost: (tf) => set({ boost: tf ? 20 : 0 }),
   resetVelocity: () => set({ velocity: 0 }),
   resetDirection: () => set({ direction: 0 }),
   setVelocity: (n) =>
     set((state) => ({
       velocity:
         n >= 0
-          ? Math.min(state.velocity + n, 1)
+          ? Math.min(state.velocity + n, 5)
           : Math.max(state.velocity + n, 0),
     })),
   setDirection: (n) =>
